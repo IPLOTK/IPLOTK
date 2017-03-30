e3105d4183d311fb63c425880238f4b052808819
pigcake999yt <999pigcake@gmail.com>
2017-03-30 18:15:53 +0100

    Add files via upload

---
+++ views/Root.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  
+  <!-- CSS -->
+  <link rel="stylesheet" href="http://iplo.tk/assets/css/styles.css">
+
+  <!-- Material Design -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <!-- Custom Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="header">
+    <span style="font-family: 'Baloo Bhaina', arial, sans-serif;position:absolute;font-size:26pt;margin:50px;">IPLO</span>
+  </div>
+</body>
+</html>
